1e729d3cd44e83d2fd45d3d777a7fc530cf4aaf7
Adarshjeet0 <adarshjeetc@gmail.com>
2025-03-26 12:40:27 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import store from './redux/store';
