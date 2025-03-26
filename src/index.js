0ee926494b4dc8b6ff54a554b1c3722867de7eb3
Adarshjeet0 <adarshjeetc@gmail.com>
2025-03-26 12:36:07 +0530

    add

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Router>
+        <Router basename="/advanced-todo-app">
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
